1539f1f639a7639d778d4cf1f3271594b9ac0423
lilbernier <brendanbernier@gmail.com>
2024-04-11 19:16:43 -0400

    a little set up

--- addons/vox_reader/scripts/generators/greedy_generator.gd
+++ addons/vox_reader/scripts/generators/greedy_generator.gd
@@ -1,7 +1,71 @@
 class_name GreedyGenerator
 
 
-static func Generate(_root, _voxData):
-	for v in _voxData.voxels:
-		await _root.get_tree().create_timer(.05).timeout
+static var surfaceTool: SurfaceTool = SurfaceTool.new()
 
+static func Generate(_root, _voxData):
+	surfaceTool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	surfaceTool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+	pass
+
+
+static var TOP = [
+	Vector3( 1.0000, 1.0000, 1.0000),
+	Vector3( 0.0000, 1.0000, 1.0000),
+	Vector3( 0.0000, 1.0000, 0.0000),
+	
+	Vector3( 0.0000, 1.0000, 0.0000),
+	Vector3( 1.0000, 1.0000, 0.0000),
+	Vector3( 1.0000, 1.0000, 1.0000),
+];
+
+static var BOTTOM = [
+	Vector3( 0.0000, 0.0000, 0.0000),
+	Vector3( 0.0000, 0.0000, 1.0000),
+	Vector3( 1.0000, 0.0000, 1.0000),
+	
+	Vector3( 1.0000, 0.0000, 1.0000),
+	Vector3( 1.0000, 0.0000, 0.0000),
+	Vector3( 0.0000, 0.0000, 0.0000),
+];
+
+static var FRONT = [
+	Vector3( 0.0000, 1.0000, 1.0000),
+	Vector3( 1.0000, 1.0000, 1.0000),
+	Vector3( 1.0000, 0.0000, 1.0000),
+	
+	Vector3( 1.0000, 0.0000, 1.0000),
+	Vector3( 0.0000, 0.0000, 1.0000),
+	Vector3( 0.0000, 1.0000, 1.0000),
+];
+
+static var BACK = [
+	Vector3( 1.0000, 0.0000, 0.0000),
+	Vector3( 1.0000, 1.0000, 0.0000),
+	Vector3( 0.0000, 1.0000, 0.0000),
+	
+	Vector3( 0.0000, 1.0000, 0.0000),
+	Vector3( 0.0000, 0.0000, 0.0000),
+	Vector3( 1.0000, 0.0000, 0.0000)
+];
+
+const LEFT = [
+	Vector3( 0.0000, 1.0000, 1.0000),
+	Vector3( 0.0000, 0.0000, 1.0000),
+	Vector3( 0.0000, 0.0000, 0.0000),
+	
+	Vector3( 0.0000, 0.0000, 0.0000),
+	Vector3( 0.0000, 1.0000, 0.0000),
+	Vector3( 0.0000, 1.0000, 1.0000),
+];
+
+const RIGHT = [
+	Vector3( 1.0000, 1.0000, 1.0000),
+	Vector3( 1.0000, 1.0000, 0.0000),
+	Vector3( 1.0000, 0.0000, 0.0000),
+	
+	Vector3( 1.0000, 0.0000, 0.0000),
+	Vector3( 1.0000, 0.0000, 1.0000),
+	Vector3( 1.0000, 1.0000, 1.0000),
+];
