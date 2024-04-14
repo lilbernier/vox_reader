582610ad16b6aac487501a9d012b1a739a10e84d
lilbernier <brendanbernier@gmail.com>
2024-04-14 13:25:39 -0400

    changing generation type names

--- addons/vox_reader/scripts/voxel_mesh.gd
+++ addons/vox_reader/scripts/voxel_mesh.gd
@@ -3,46 +3,36 @@ class_name VoxelMesh extends Node
 @export var file_path:String
 @export var file_name:String
 var voxData = null
-@export_enum("Unoptimized", "Greedy", "Culled") var generation_type: int
+@export var generation_type: GEN_TYPE
 
 @export var voxel_scale = .1;
 
 @export var collisions: bool = true
 
-@export var animated: bool = false
-
+enum GEN_TYPE{
+	GREEDY,
+	CUBE
+}
 
 func _ready():
 	voxData = VoxelImporter.open(file_path + file_name, null)
 	var time_start = Time.get_ticks_msec()
 
-
 	match(generation_type):
-		0: #Unoptimized
+		GEN_TYPE.GREEDY: #GREEDY
+			#for i in 200:
+				#print('Greedy mesh.')
+			GreedyGenerator.Generate(self, voxData, file_name)
+
+		GEN_TYPE.CUBE: #CUBE
 			print('Unoptimized mesh.')
 			UnoptimizedGenerator.Generate(self, voxData)
 			#for v in voxData.voxels:
 				#await get_tree().create_timer(.05).timeout
 				#generate_block(v, voxData.voxels[v])
-		1:
-			#for i in 200:
-				#print('Greedy mesh.')
-			GreedyGenerator.Generate(self, voxData, file_name)
-		2:
-			pass
-			#print('Culled mesh.')
 		_:
 			print('Somehow you messed up the generation type')
 	
 	var time_now = Time.get_ticks_msec()
 	var time_elapsed = time_now - time_start
 	print('GENERATOR TYPE: ' + str(generation_type) + ' ' + str(time_elapsed))
-	
-func generate_block(_position, _colorIndex):
-	var cube = MeshInstance3D.new()
-	cube.mesh = BoxMesh.new()
-	cube.position = _position * voxel_scale;
-	cube.mesh.material = StandardMaterial3D.new()
-	cube.mesh.material.albedo_color = voxData.colors[_colorIndex]
-	cube.scale = cube.scale * voxel_scale
-	add_child(cube)
